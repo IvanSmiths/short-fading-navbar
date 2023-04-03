d2fbf443049a3046beaf7edd37201fd5800ce229
IvanSmiths <info@ivansmiths.com>
2023-04-03 20:40:35 +0200

    add fading animation to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,29 @@
+import { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
 function Navbar() {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const navbarRef = useRef(null)
+
+    useEffect(() => {
+        const showNav = gsap.fromTo(navbarRef.current, {
+            opacity: 0,
+        }, {
+            opacity: 1,
+            duration: 0.4
+        }).progress(1);
+        ScrollTrigger.create({
+            start: "top top",
+            end: "max",
+            onUpdate: (self) => {
+                self.direction === -1 ? showNav.play() : showNav.reverse()
+            }
+        });
+    }, [])
     return (
-        <nav className='navbar'>
+        <nav ref={navbarRef} className='navbar'>
             <span>Home</span>
             <div className='hamburger'>
                 <div></div>
