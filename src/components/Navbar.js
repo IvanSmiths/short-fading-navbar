2734cacc5c16348ddd79488ed498d6e5d0b49fad
IvanSmiths <info@ivansmiths.com>
2023-04-03 20:38:50 +0200

    add navbar and style

---
+++ src/components/Navbar.js
@@ -0,0 +1,14 @@
+
+function Navbar() {
+    return (
+        <nav className='navbar'>
+            <span>Home</span>
+            <div className='hamburger'>
+                <div></div>
+                <div></div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,4 @@
+import Navbar from '@/components/Navbar'
 import { Roboto } from 'next/font/google'
 
 const roboto = Roboto({
@@ -8,6 +9,7 @@ const roboto = Roboto({
 export default function Home() {
   return (
     <main className={roboto.className}>
+      <Navbar />
       <div className='hero'>
         <h1>Lorem ipsum dolor sit amet</h1>
         <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero commodi consequatur numquam quis explicabo corrupti nostrum voluptate a deserunt alias!</p>
