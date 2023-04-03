fd616ef596991730a7a0cd6970da4a2530ce5d14
IvanSmiths <info@ivansmiths.com>
2023-04-03 20:36:41 +0200

    add placeholders

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,19 @@
+import { Roboto } from 'next/font/google'
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <main className={roboto.className}>
+      <div className='hero'>
+        <h1>Lorem ipsum dolor sit amet</h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Vero commodi consequatur numquam quis explicabo corrupti nostrum voluptate a deserunt alias!</p>
+        <span>Click me</span>
+      </div>
+      <p className='big'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellat doloribus rem optio odio ab, obcaecati quam velit! Neque aliquam eius illo, incidunt suscipit tenetur placeat nisi et nobis, aspernatur culpa magni saepe. Maiores esse earum quibusdam quos eum quod magnam illo maxime? Dolores velit culpa officia. Iusto minima voluptates, omnis fugiat dolorum quae consequuntur expedita nostrum quis illo impedit architecto?</p>
+    </main>
   )
 }
